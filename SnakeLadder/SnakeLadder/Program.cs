d850fc46afd353be549d608d7812905cfa86ab8e
unknown <nandiniswaraj13@gmail.com>
2021-03-14 19:28:44 +0530

    [Nandini swaraj]Added Dice rolled

--- SnakeLadder/SnakeLadder/Program.cs
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -4,13 +4,19 @@ namespace SnakeLadder
 {
     class Program
     {
-        int position = 0;
         static void Main(string[] args)
         {
+            int position = 0;
+            int dice;
+
+            SnakeLadderUtility snakeLadder = new SnakeLadderUtility();
             Console.WriteLine("=====================================");
             Console.WriteLine("***** Welcome Snake Ladder Game *****");
             Console.WriteLine("=====================================");
             Console.WriteLine("-----------For Single Player Mode------------");
+            Console.WriteLine("Player Starting Position Is: "+position);
+            dice = snakeLadder.diceRoll();
+            Console.WriteLine("Player Rolled Dice And Obtained Number: "+dice);
         }
     }
 }
---
+++ SnakeLadder/SnakeLadder/SnakeLadderUtility.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadder
+{
+    public class SnakeLadderUtility
+    {
+        
+        public  int diceRoll()
+        {
+            Random random = new Random();
+            int dice = random.Next(0, 7);
+            return dice;
+        }
+    }
+}
