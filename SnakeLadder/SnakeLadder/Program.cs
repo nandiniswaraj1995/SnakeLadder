0c5b9cc1a6ed4757907ad9fa435f303c49f3e91d
unknown <nandiniswaraj13@gmail.com>
2021-03-14 19:14:14 +0530

    [Nandini swaraj]Added Single player mode

---
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    class Program
+    {
+        int position = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=====================================");
+            Console.WriteLine("***** Welcome Snake Ladder Game *****");
+            Console.WriteLine("=====================================");
+            Console.WriteLine("-----------For Single Player Mode------------");
+        }
+    }
+}
